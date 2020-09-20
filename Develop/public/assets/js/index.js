51e62ee3b36530536a694e432ab5047cb7d934dd
Caroline Twyman <carolinetwyman@gmail.com>
2020-09-19 20:22:57 -0500

    removing console logs

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -53,7 +53,7 @@ const renderActiveNote = () => {
 
 // Get the note data from the inputs, save it to the db and update the view
 const handleNoteSave = function () {
-  console.log("saveisrunning")
+  //console.log("saveisrunning")
   const newNote = {
     title: $noteTitle.val(),
     text: $noteText.val(),
